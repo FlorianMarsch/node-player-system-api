e3eaffc5cc5876183253daaaf12079d1e8c33de5
Florian Marsch <florianmarsch@Florians-MacBook-Pro.local>
2017-09-01 20:22:08 +0200

    add lineUp

--- models/squad.js
+++ models/squad.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('Squad',{
 	id: String,
 	players :{ type: [{ type: mongoose.Schema.ObjectId, ref: 'Player' }], default: [] },
+	lineUp :{ type: [{ type: mongoose.Schema.ObjectId, ref: 'Player' }], default: [] },
 	money :{ type: Number, default: 40000000 },
 	ownerId : { type: String, required:true},
 	updated: { type: Date, default: Date.now }

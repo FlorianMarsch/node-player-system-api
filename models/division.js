c58e53fe4815ffdcf2240a4dee584e4e9e40d10a
Florian Marsch <florianmarsch@Florians-MacBook-Pro.local>
2018-01-31 22:46:55 +0100

    pre save

--- models/division.js
+++ models/division.js
@@ -13,10 +13,18 @@ var autoPopulate = function(next) {
     this.populate('member');
     next();
   };
+
+  var autoReduce = function(next) {
+    if(this.member){
+		this.member = this.member.map(function(element){return element._id});
+	}
+    next();
+  };
   
 schema.
     pre('findOne', autoPopulate).
-    pre('find', autoPopulate);
+	pre('find', autoPopulate).
+	pre('save',autoReduce);
 
 
 
